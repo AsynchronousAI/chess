d17012f9fd53f3e6786553608e0d4966f05c89d7
Aarav Sethi <aaravs@Aaravs-Mac-Studio.local>
2025-07-31 16:15:42 -0500

    Keybind to flip board.
    Signed-off-by: Aarav Sethi <aaravs@Aaravs-Mac-Studio.local>

--- src/client/stories/board.story.tsx
+++ src/client/stories/board.story.tsx
@@ -6,10 +6,19 @@ import { RootProvider } from "client/providers/root-provider";
 import Board from "client/components/game/board";
 import { Menu } from "client/components/menu";
 import { Stats } from "client/components/stats";
+import { InputCapture } from "client/components/ui/input-capture";
+import Atoms from "shared/atoms";
 
 export = hoarcekat(() => {
 	return (
 		<RootProvider>
+			<InputCapture
+				onInputBegan={(_, input) => {
+					if (input.KeyCode === Enum.KeyCode.G) {
+						Atoms.PlayingAs((prev) => (prev === "white" ? "black" : "white"));
+					}
+				}}
+			/>
 			<Stats />
 			<Board />
 		</RootProvider>
